9c6bea97546de949b9eafcc0b8111f7bd0163a32
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-02-11 00:24:32 +0700

    fix vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/test-vue/'
+    ? '/todo/'
     : '/',
   pluginOptions: {
     'style-resources-loader': {
